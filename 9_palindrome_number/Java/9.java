f290edecc0d05e57bb7c27c5aade0e63bec1e703
Andrei Stan <andrei_stan_2000@yahoo.com>
2023-09-08 13:47:11 +0300

    9 C++ Java O(log10 n)

---
+++ 9_palindrome_number/Java/9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int name() {
+        return 9;
+    }
+}
+
+class P9 {
+    public static void main(String args[]) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.name());
+    }
+}
