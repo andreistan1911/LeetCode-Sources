6e76eb80676e89348b2dc5b80166a01643130706
Andrei Stan <andrei_stan_2000@yahoo.com>
2023-09-08 13:27:09 +0300

    Added Java Template

---
+++ Templates/Java/template.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int name() {
+        return 0;
+    }
+}
+
+public class template {
+    public static void main(String args[]) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.name());
+    }
+}
