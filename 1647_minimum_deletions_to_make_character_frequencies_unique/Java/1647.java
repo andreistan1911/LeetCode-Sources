0e92b5cdb1d8176538db0e503c32f5f31a3f5188
Andrei Stan <andrei_stan_2000@yahoo.com>
2023-09-12 18:00:52 +0300

    1647 Java 7ms 98.85%

---
+++ 1647_minimum_deletions_to_make_character_frequencies_unique/Java/1647.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    /**
+     * Calculates the minimum number of deletions required to make all characters in the given string `s` unique.
+     *
+     * @param s The input string
+     * @return The minimum number of deletions required
+     */
+    public int minDeletions(String s) {
+        int answer = 0;
+        int[] frequencies = new int[26];
+
+        // Count the frequencies of each character in the string
+        for (char c : s.toCharArray()) {
+            frequencies[c - 'a']++;
+        }
+
+        // Sort the frequencies in ascending order
+        Arrays.sort(frequencies);
+
+        // Iterate through the frequencies array and check if any frequency is greater than or equal to the next frequency
+        for (int i = 24; i >= 0; i--) {
+            while (frequencies[i] != 0 && frequencies[i] >= frequencies[i + 1]) {
+                frequencies[i]--;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
+
+class P1647 {
+    public static void main(String args[]) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.minDeletions("aaabc"));
+    }
+}
